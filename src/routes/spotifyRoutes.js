db4ce50a8f7e1a74c32a63435f38de4435faeb1f
Baajike <baajikeimmanuella@gmail.com>
2024-05-23 15:52:19 -0700

    update routes

--- src/routes/spotifyRoutes.js
+++ src/routes/spotifyRoutes.js
@@ -1,8 +1,19 @@
 const express = require('express');
 const router = express.Router();
-const spotifyController = require('../controllers/spotifyController');
+const axios = require('axios');
+const querystring = require('querystring');
 
-router.get('/search', spotifyController.searchTracks);
+router.get('/login', (req, res) => {
+    const scope = 'user-read-private user-read-email';
+    const params = querystring.stringify({
+        response_type: 'code',
+        client_id: process.env.SPOTIFY_CLIENT_ID,
+        scope: scope,
+        redirect_uri: process.env.REDIRECT_URI,
+    });
+
+    res.redirect(`https://accounts.spotify.com/authorize?${params}`);
+});
 
 module.exports = router;
 

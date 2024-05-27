f85a47334c4fe1a755080d76b08f0b52ac669a72
Baajike <baajikeimmanuella@gmail.com>
2024-05-26 21:02:05 -0700

    code fixed

--- src/controllers/spotifyController.js
+++ src/controllers/spotifyController.js
@@ -1,15 +1,28 @@
 const axios = require('axios');
 
-exports.getSpotifyData = async (req, res) => {
+const searchTracks = async (req, res) => {
+  const { query } = req.query;
+
+  if (!query) {
+    return res.status(400).json({ error: 'Query parameter is required' });
+  }
+
   try {
-    const response = await axios.get('https://api.spotify.com/v1/me/top/tracks', {
+    const response = await axios.get('https://api.spotify.com/v1/search', {
       headers: {
-        Authorization: `Bearer ${process.env.SPOTIFY_ACCESS_TOKEN}`
-      }
+        Authorization: `Bearer ${process.env.SPOTIFY_ACCESS_TOKEN}`,
+      },
+      params: {
+        q: query,
+        type: 'track',
+      },
     });
+
     res.json(response.data);
   } catch (error) {
     res.status(500).json({ error: 'Failed to fetch data from Spotify' });
   }
 };
+
+module.exports = { searchTracks };
 
